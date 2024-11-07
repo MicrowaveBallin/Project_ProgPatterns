a3e9d5465746629ec2e848e91f0828357a591606
UAlaina <urmicaa@gmail.com>
2024-11-06 19:30:37 -0500

    sql part

---
+++ MovieBookingSystem/src/main/java/org/example/model/Admin.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+public class Admin extends User {
+    void addMovies(){
+
+    }
+
+    void selectMovies(){
+
+    }
+}
---
+++ MovieBookingSystem/src/main/java/org/example/model/User.java
@@ -0,0 +1,21 @@
+package org.example.model;
+
+public class User {
+    String name,username, password, address, phone;
+
+    void buyTicket(){
+
+    }
+
+    void showTicket(){
+
+    }
+
+    void cancelTicket(){
+
+    }
+
+    void logout(){
+
+    }
+}
